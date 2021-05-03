1c9de474203f6bea19f1546f423d4e8577ce0322
apurvi96 <purvi.ag96@gmail.com>
2021-05-04 01:21:04 +0530

    Final changes

--- match.py
+++ match.py
@@ -4,16 +4,17 @@ import json
 url = "http://127.0.0.1:5000/match_updates"
 
 def getUpdates():
-	match = input("Enter Match Name")
-	player = input("Enter Player Name")
-	score = input("Enter score to be added")
+	print("-----------------------------------------------------------------------")
+	match = input("Enter Match Name: ")
+	player = input("Enter Player Name: ")
+	score = input("Enter score to be added: ")
 	sendUpdates(match, player, score)
 
-def sendUpdates(match, player, updates):
+def sendUpdates(match, player, score):
 	updates = {'match':match, 'player':player, 'score':score}
-	result = requests.get(url, params=match_updates).json()
+	result = requests.get(url, params=updates).json()
 	return result
 
 if '__name__ == __main__':
 	while(1):
-		getUpdates
+		getUpdates()
